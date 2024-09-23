3ad3ab776890169ffbeffae82cb753ab596afdb4
kohyeokjea <kohj0686@naver.com>
2024-09-23 21:52:54 +0900

    자바스크립트 12일차

---
+++ 19-asynchronous/19-01-synchronous.js
@@ -0,0 +1,15 @@
+// 동기적 실행
+// V8: 싱글스레드로 작동
+// 지연 - blocking -> 비동기적 실행 필요
+console.log("코드1");
+
+setTimeout(() => console.log("코드2"), 0) // 비동기 함수 non-blocking
+
+console.log("코드3");
+
+/**
+ * [Web APIs]
+ * - setTimeout, fetch, DOM 등
+ * - JavaScripth V8 엔진과 별개로 동작
+ * - 마치 멀티스레드 처럼 동작
+ */
